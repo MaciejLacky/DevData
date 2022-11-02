c5d8524281ce0983451e69b8dd0d177e7e9d7c39
Maciej Łącki <maciej.lacky@gmail.com>
2022-11-02 22:23:15 +0100

    add view models and view prepare command for buttons

---
+++ DevData/ViewModels/WorkMainPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using DevData.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DevData.ViewModels
+{
+    public class WorkMainPageViewModel
+    {
+        #region Fields
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public ICommand TestConnectionCommand { get; set; }
+        #endregion
+
+        #region Constructor
+        public WorkMainPageViewModel()
+        {
+            TestConnectionCommand = new RelayCommand(TestConnectionDb);
+        }
+        #endregion
+
+        #region Methods
+        private void TestConnectionDb()
+        {
+            var lg = Login;
+            var pass = Password;
+        }
+        private void GetAllData()
+        {
+
+        }
+        #endregion
+
+    }
+}
