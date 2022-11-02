fd208d4074b4dec9499e04da1b64601fb14e1d7e
Maciej Łącki <maciej.lacky@gmail.com>
2022-11-02 21:11:39 +0100

    Add project files.

---
+++ DevData/Models/Table_A.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevData.Models
+{
+    public class Table_A
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Col_A1 { get; set; }
+        public string Col_A2 { get; set; }
+        public DateTime Col_A3 { get; set; }
+    }
+}
